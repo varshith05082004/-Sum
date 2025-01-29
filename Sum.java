047dbf447ed6b43b7fe0eef3b4d516a25fed11de
varshith05082004 <varshithreddybobbali@gmail.com>
2025-01-29 22:18:32 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,16 @@
+public class Sum{
+    public static void main(String[] args) {
+        int sum=0;
+        int i=1;
+        while(i<=100){
+            if(i%2==0){
+                sum=sum+i;
+            }
+            i++;
+        }System.out.println(sum);
+    }
+}
+
+
+
+
